a000d4c0c4a05ba3a8b00d738fd7d1f52e3c0524
Eric Fithian <ef2413@columbia.edu>
2017-06-09 21:10:56 -0400

    Updated routes, controllers

--- SiteForDad/config/routes.rb
+++ SiteForDad/config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  as :user do
+    get '/schools/sign_up', to: 'users/registrations#new_school'
+    post '/users/registrations/', to: 'users/registrations#import'
+  end
+
+  resources :applications, only: [:index, :new, :show, :create, :edit, :update] do
+    resources :products, only: [:index, :new, :create, :edit, :update]
+    resources :work_experiences, only: [:index, :new, :create, :edit, :update]
+    resources :references, only: [:index, :new, :create, :edit, :update]
+    resources :documents, only: [:index, :new, :create, :edit, :update]
+  end
 end
