57b95b11d47c5f5f3b133782acc6d739f4577df3
Eric Fithian <ef2413@columbia.edu>
2017-07-02 01:52:15 -0400

    Added Workshop, Charge controllers

--- Liz_Education_Site/config/routes.rb
+++ Liz_Education_Site/config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  devise_for :users, path: '', controllers: {registrations: 'users/registrations'}
-  as :user do
-    post '/users/registrations/', to: 'users/registrations#import'
-  end
+  # devise_for :users, path: '', controllers: {registrations: 'users/registrations'}
+  # as :user do
+  #   post '/users/registrations/', to: 'users/registrations#import'
+  # end
   root :to => 'pages#index'
   get "/about" => 'pages#about'
   get "/contact" => 'pages#contact'
@@ -12,28 +12,23 @@ Rails.application.routes.draw do
 
   resources :applications, only: [:index, :new, :show, :create, :edit, :update] do
     put :payment
-    resources :educations, only: [:index, :new, :create, :edit, :update]
-    resources :montessori_trainings, only: [:index, :new, :create, :edit, :update]
-    resources :work_experiences, only: [:index, :new, :create, :edit, :update]
-    resources :references, only: [:index, :new, :create, :edit, :update]
-    resources :documents, only: [:index, :new, :create, :edit, :update]
   end
 
-  resources :student_profiles, only: [:index, :create]
+  # resources :student_profiles, only: [:index, :create]
 
-  get "/student_profiles/edit" => "student_profiles#edit"
-  patch "/student_profiles" => "student_profiles#update"
+  # get "/student_profiles/edit" => "student_profiles#edit"
+  # patch "/student_profiles" => "student_profiles#update"
 
-  resources :school_profiles, only: [:index, :new, :create, :edit, :update] do
-  end
+  # resources :school_profiles, only: [:index, :new, :create, :edit, :update] do
+  # end
 
-  resources :local_schools, only: [:index, :new, :create, :edit, :update]
-  post "/registrants/import", to: "registrants#import"
-  resources :registrants
+  # resources :local_schools, only: [:index, :new, :create, :edit, :update]
+  # post "/registrants/import", to: "registrants#import"
+  # resources :registrants
   
 
-  resources :workshops
-  resources :charges
-  resources :courses
-  resources :course_registrations
+  # resources :workshops
+  # resources :charges
+  # resources :courses
+  # resources :course_registrations
 end
