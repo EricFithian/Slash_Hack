9c685eaf445b5bb8540907b0cc9f87fa57d801c8
Eric Fithian <ef2413@columbia.edu>
2017-07-12 19:23:02 -0400

    Added contact model

--- NextKidEntrepreneur/db/schema.rb
+++ NextKidEntrepreneur/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170712003614) do
+ActiveRecord::Schema.define(version: 20170712232224) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,15 @@ ActiveRecord::Schema.define(version: 20170712003614) do
     t.string   "description"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "phone_number"
+    t.string   "question"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "school_profiles", force: :cascade do |t|
