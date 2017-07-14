4a20cf68cd3e0148ca1479d807b16a1debda276a
Eric Fithian <ef2413@columbia.edu>
2017-07-14 18:38:15 -0400

    Updated gemfile

--- NextKidEntrepreneur/app/controllers/application_controller.rb
+++ NextKidEntrepreneur/app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def after_sign_in_path_for(resource)
+    if current_user.admin > 0
+      admin_path
+    elsif current_user.admin > 1
+      super_admin_path
+    else
+      user_path
+    end
+  end
 end
