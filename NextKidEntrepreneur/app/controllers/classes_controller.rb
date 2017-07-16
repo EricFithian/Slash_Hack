3a9394260246b5ead9d6915712139e4344a7b94e
Eric Fithian <ef2413@columbia.edu>
2017-07-15 23:30:43 -0400

    Updated classes controller

--- NextKidEntrepreneur/app/controllers/classes_controller.rb
+++ NextKidEntrepreneur/app/controllers/classes_controller.rb
@@ -1,2 +1,11 @@
 class ClassesController < ApplicationController
+  skip_before_action :authenticate_user!
+
+  def index
+    @class = Class.all
+  end
+  
+  def show
+    @class = Class.find(params[:id])
+  end
 end
