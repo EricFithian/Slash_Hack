fa94f0e558ce2b4cadb72014ab55e304480c0e0d
Eric Fithian <ef2413@columbia.edu>
2017-07-20 23:38:42 -0400

    Updated schools controller

--- Liz_Education_Site/app/controllers/schools_controller.rb
+++ Liz_Education_Site/app/controllers/schools_controller.rb
@@ -1,2 +1,28 @@
 class SchoolsController < ApplicationController
+  def index
+    @school = current_user.school
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+    @school = current_user.school
+  end
+
+  def update
+    school = current_user.school
+    school.assign_attributes(school_params)
+    school.save
+    redirect_to "/schools"
+  end
+
+  private
+
+  def school_params
+    params.require(:school).permit(:contact_name, :school_address, :bio)
+  end
 end
