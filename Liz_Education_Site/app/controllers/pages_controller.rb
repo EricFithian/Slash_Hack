e9b7b60181cd826be586fb58572858d9180944c0
Eric Fithian <ef2413@columbia.edu>
2017-07-21 22:48:34 -0400

    Added and edited pages controller

---
+++ Liz_Education_Site/app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+  skip_before_action :authenticate_user!
+
+  def index
+
+  end
+
+  def about
+    @title = "About Us"
+  end
+
+  def contact
+    @title = "Contact"
+  end
+
+  # def template
+  #   @title = "Example Title"
+  # end
+end
