b2d9211d73aceb5d0ae655b5cbc9b01c6ee66b09
Eric Fithian <ef2413@columbia.edu>
2017-07-23 23:51:08 -0400

    Updated courses controller and views

--- Liz_Education_Site/app/controllers/courses_controller.rb
+++ Liz_Education_Site/app/controllers/courses_controller.rb
@@ -1,2 +1,15 @@
 class CoursesController < ApplicationController
+    skip_before_action :authenticate_user!
+  
+  def index
+    @courses = Course.all
+  end
+
+  def new
+    @courses = Course.all
+  end
+  
+  def show
+    @course = Course.find(params[:id])
+  end
 end
