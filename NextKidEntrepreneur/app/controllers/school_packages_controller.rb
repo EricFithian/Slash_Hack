93ff87375cea19c124d951a36e119fd1288861dc
Eric Fithian <ef2413@columbia.edu>
2017-08-21 23:48:57 -0400

    Added school packages controller

---
+++ NextKidEntrepreneur/app/controllers/school_packages_controller.rb
@@ -0,0 +1,2 @@
+class SchoolPackagesController < ApplicationController
+end
---
+++ NextKidEntrepreneur/spec/helpers/school_packages_helper_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+# Specs in this file have access to a helper object that includes
+# the SchoolPackagesHelper. For example:
+#
+# describe SchoolPackagesHelper do
+#   describe "string concat" do
+#     it "concats two strings with spaces" do
+#       expect(helper.concat_strings("this","that")).to eq("this that")
+#     end
+#   end
+# end
+RSpec.describe SchoolPackagesHelper, type: :helper do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
